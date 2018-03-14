0904b54b81bf52ae9bd88e5edb416f01e7038dca
Ece Asilar <easilar@gmail.com>
2018-03-14 12:58:37 +0100

    finalversion

---
+++ PhD_Thesis_Fin/Plots/analysis/control_Plots/cp.sh
@@ -0,0 +1,16 @@
+#scp -r easilar@heplx10.oeaw.ac.at://afs/hephy.at/user/e/easilar/www/Moriond2017/plots/_mu_dPhiltp5_isoVeto/st250_ht500_njet5_nbtagEq0 mu/
+#scp -r easilar@heplx10.oeaw.ac.at://afs/hephy.at/user/e/easilar/www/Moriond2017/plots/_ele_dPhiltp5_isoVeto/st250_ht500_njet5_nbtagEq0 ele/
+#scp -r easilar@heplx10.oeaw.ac.at://afs/hephy.at/user/e/easilar/www/Moriond2017/plots/_ele__isoVeto/st250_ht500_njet4-5_nbtag1 ele/
+#scp -r easilar@heplx10.oeaw.ac.at://afs/hephy.at/user/e/easilar/www/Moriond2017/plots/_mu__isoVeto/st250_ht500_njet4-5_nbtag1 mu/
+#scp -r easilar@heplx10.oeaw.ac.at://afs/hephy.at/user/e/easilar/www/Moriond2017/plots/_mu__isoVeto/st250_ht500_njet3-4_nbtagEq0 mu/
+#scp -r easilar@heplx10.oeaw.ac.at://afs/hephy.at/user/e/easilar/www/Moriond2017/plots/_ele__isoVeto/st250_ht500_njet3-4_nbtagEq0 ele/
+
+cp /afs/hephy.at/user/e/easilar/www/Moriond2017/plots_AN_v2/_ele__isoVeto/st250_ht500_njet3-4_nbtagEq0/* /afs/cern.ch/user/e/easilar/public/html/Notes/notes/AN-16-413/trunk/plots/zerob/ControlPlots/ele/st250_ht500_njet3-4_nbtagEq0
+cp /afs/hephy.at/user/e/easilar/www/Moriond2017/plots_AN_v2/_ele__isoVeto/st250_ht500_njet4-5_nbtag1/* /afs/cern.ch/user/e/easilar/public/html/Notes/notes/AN-16-413/trunk/plots/zerob/ControlPlots/ele/st250_ht500_njet4-5_nbtag1
+cp /afs/hephy.at/user/e/easilar/www/Moriond2017/plots_AN_v2/_ele__isoVeto/st250_ht500_njet5_nbtagEq0/* /afs/cern.ch/user/e/easilar/public/html/Notes/notes/AN-16-413/trunk/plots/zerob/ControlPlots/ele/st250_ht500_njet5_nbtagEq0
+cp /afs/hephy.at/user/e/easilar/www/Moriond2017/plots_AN_v2/_mu__isoVeto/st250_ht500_njet3-4_nbtagEq0/* /afs/cern.ch/user/e/easilar/public/html/Notes/notes/AN-16-413/trunk/plots/zerob/ControlPlots/mu/st250_ht500_njet3-4_nbtagEq0
+cp /afs/hephy.at/user/e/easilar/www/Moriond2017/plots_AN_v2/_mu__isoVeto/st250_ht500_njet4-5_nbtag1/* /afs/cern.ch/user/e/easilar/public/html/Notes/notes/AN-16-413/trunk/plots/zerob/ControlPlots/mu/st250_ht500_njet4-5_nbtag1
+cp /afs/hephy.at/user/e/easilar/www/Moriond2017/plots_AN_v2/_mu__isoVeto/st250_ht500_njet5_nbtagEq0/* /afs/cern.ch/user/e/easilar/public/html/Notes/notes/AN-16-413/trunk/plots/zerob/ControlPlots/mu/st250_ht500_njet5_nbtagEq0
+
+
+
